30b53acf24cf6642a98dc25620022fed9a87ea3b
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-08-13 15:59:52 -0400

    Updates

---
+++ precache-manifest.b198fd3fee246be5eb7a9924d6d84594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbeeb9516af174ff166ec241082c2a6",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "c98877809283048017fa",
+    "url": "/projects/static/css/main.217d9b4d.chunk.css"
+  },
+  {
+    "revision": "671fa3d8c60dace6e01b",
+    "url": "/projects/static/js/2.c0b01071.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projects/static/js/2.c0b01071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98877809283048017fa",
+    "url": "/projects/static/js/main.4debd70a.chunk.js"
+  },
+  {
+    "revision": "f639c188cb940516bcf1",
+    "url": "/projects/static/js/runtime-main.da0864a9.js"
+  }
+]);
