a2495e8398a2d71118d93e572449e4cbd9e61db0
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-08-14 20:30:45 -0400

    Use separate span for ⚗
    symbol for ongoing projects

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -9,7 +9,10 @@ class Project extends React.Component {
     return (
       <div className="card">
         <div className="card-body">
-          <h2 className="card-title">{this.props.name + (this.props.ongoing ? ' ⚗' : '')}</h2>
+          <h2 className="card-title">
+            {this.props.name}
+            {this.props.ongoing ? <span title="ongoing" className="help-cursor no-select">&nbsp;⚗</span> : <></>}
+          </h2>
           <p className="card-text">{this.props.caption}</p>
           <small className="text-muted">{this.props.lang}</small>
         </div>
