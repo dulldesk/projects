01ca8313540f7c9ccc2cdc9a50367dcb20d7de5a
dulldesk <dulldesk@users.noreply.github.com>
2020-09-14 01:55:14 +0000

    deploy: 6b48ba732f93865e531ad7e542d786cd8b8873e7

--- precache-manifest.72ce912adf31e2e02c55de405f2a800e.js
+++ precache-manifest.72ce912adf31e2e02c55de405f2a800e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f6d1f8a604afe8027caec426059ab88d",
+    "revision": "8c080c2bc1b29c6eaebe159ffff24671",
     "url": "/projects/index.html"
   },
   {
-    "revision": "b1c52911b0f4f2ca2fff",
+    "revision": "301fa7414a03bc997d86",
     "url": "/projects/static/css/main.4fa0f505.chunk.css"
   },
   {
-    "revision": "27e8690bac4c3a7e4a16",
-    "url": "/projects/static/js/2.75e92eb2.chunk.js"
+    "revision": "6ab18b48d0fa975d4431",
+    "url": "/projects/static/js/2.b3df4cc1.chunk.js"
   },
   {
     "revision": "5ced2f2ce7b79285485a360b83230d1b",
-    "url": "/projects/static/js/2.75e92eb2.chunk.js.LICENSE.txt"
+    "url": "/projects/static/js/2.b3df4cc1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b1c52911b0f4f2ca2fff",
-    "url": "/projects/static/js/main.39a950e4.chunk.js"
+    "revision": "301fa7414a03bc997d86",
+    "url": "/projects/static/js/main.cd0e43e6.chunk.js"
   },
   {
     "revision": "f639c188cb940516bcf1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/projects/precache-manifest.855c7ac0dc49f0442a527600657f5109.js"
+  "/projects/precache-manifest.72ce912adf31e2e02c55de405f2a800e.js"
 );
 
 self.addEventListener('message', (event) => {
