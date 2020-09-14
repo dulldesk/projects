6b48ba732f93865e531ad7e542d786cd8b8873e7
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-09-13 21:50:46 -0400

    Add project data
    Previous location was in another repo

--- src/components/ProjectListing/ProjectListing.js
+++ src/components/ProjectListing/ProjectListing.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import Card from '../Card/Card';
 import CardColumns from 'react-bootstrap/CardColumns';
+import work from '../../data/project-data.json';
 
 class ProjectListing extends React.Component {
-	constructor() {
-		super();
-		this.state = {work:{}};
-	}
-	async componentDidMount() {
-		let listing = await fetch("https://dulldesk.github.io/js/projects-list.json").then(resp => resp.json());
-		this.setState({work: listing});
-	}
 	render() {
 		return (
 			<CardColumns id="node-cntr">
-				{ Object.entries(this.state.work).map(data => {
+				{ Object.entries(work).map(data => {
 					data[1].id = data[0];
 					return <Card {...data[1]} key={data[1].id} />
 				})}
